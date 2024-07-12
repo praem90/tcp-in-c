64cb7cacd451dc348f466710bb2afd7e92288f28
Mohan Raj <praem1990@gmail.com>
2024-07-12 17:24:30 +0530

    Intial commit

---
+++ Makefile
@@ -0,0 +1,19 @@
+CC=gcc
+CFLAGS=-I.
+DEPS =
+OBJ = server.o listener.o
+
+server: server.o
+	$(CC) -o build/server server.o $(CFLAGS)
+
+listener: listener.o
+	$(CC) -o build/listener listener.o $(CFLAGS)
+
+%.o: %.c $(DEPS)
+	$(CC) -c -o $@ $< $(CFLAGS)
+
+build: server listener
+
+clean:
+	rm -r build/*
+
---
+++ listener.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define SA struct sockaddr
+
+void read_msg(int client) {
+    char buff[80];
+
+    recv(client, buff, sizeof(buff), 0);
+
+    printf("Client says %s", buff);
+}
+
+int main() {
+    struct sockaddr_in remote, client;
+    int isocket;
+
+    isocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (isocket == -1) {
+        printf("Could not create socket");
+        return 1;
+    }
+    printf("Socket has been created \n");
+
+    remote.sin_family = AF_INET;
+    remote.sin_addr.s_addr = inet_addr("127.0.0.1");
+    remote.sin_port = 9899;
+
+    printf("Preparing to bind the socket to address \n");
+    if(bind(isocket, (SA*) &remote, sizeof(remote)) < 0) {
+        printf("Could not bound to socket");
+        return 1;
+    }
+    printf("Bounded to the ip");
+
+    if (listen(isocket, 5) != 0) {
+        printf("Unable to listen to the socket");
+        return 1;
+    }
+
+    printf("Listening for clients");
+
+    socklen_t len = sizeof(client);
+    int csocket = accept(isocket, (SA*) &client, &len);
+    if (csocket!= 0) {
+        printf("Unable to accept a client");
+        return 1;
+    }
+
+    char client_ip[INET_ADDRSTRLEN];
+
+    inet_ntop(AF_INET, &client.sin_addr.s_addr, client_ip, len);
+
+    printf("Connected to a client %s", client_ip);
+
+    read_msg(csocket);
+}
+
+
+
